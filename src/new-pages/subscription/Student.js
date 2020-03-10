0f8e925c463fed23485c6baafe1ace9a451f135e
Joshua Oluikpe <joshua.o@softcom.ng>
2020-03-10 11:41:01 +0100

    Subscription

--- src/new-pages/subscription/Student.js
+++ src/new-pages/subscription/Student.js
@@ -1,94 +1,95 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
-import PricingItem from 'components/CleanUIComponents/PricingItem';
-import { Table, Row, Col } from 'antd';
-import styles from './style.module.scss';
+import React from "react";
+import { Helmet } from "react-helmet";
+import PricingItem from "components/CleanUIComponents/PricingItem";
+import { Table, Row, Col } from "antd";
+import styles from "./style.module.scss";
 
 const Students = () => {
-	const columns = [
-		{
-			title: '',
-			dataIndex: 'row',
-			key: 'row',
-		},
-		{
-			title: 'Monthly',
-			dataIndex: 'monthly',
-			key: 'monthly',
-			render: text => <a>{text}</a>,
-		},
-		{
-			title: 'Semester',
-			dataIndex: 'semester',
-			key: 'semester',
-			render: text => <a>{text}</a>,
-		},
-		{
-			title: 'Session',
-			dataIndex: 'session',
-			key: 'session',
-			render: text => <a>{text}</a>,
-		},
-	];
+  const columns = [
+    {
+      title: "",
+      dataIndex: "row",
+      key: "row"
+    },
+    {
+      title: "Monthly",
+      dataIndex: "monthly",
+      key: "monthly",
+      render: text => <a>{text}</a>
+    },
+    {
+      title: "Semester",
+      dataIndex: "semester",
+      key: "semester",
+      render: text => <a>{text}</a>
+    },
+    {
+      title: "Session",
+      dataIndex: "session",
+      key: "session",
+      render: text => <a>{text}</a>
+    }
+  ];
 
-	const data = [
-		{
-			key: '1',
-			row: 'Cost',
-			monthly: 'NGN1,500',
-			semester: 'NGN6,000',
-			session: 'NGN10,000',
-		},
-		{
-			key: '2',
-			row: 'Discount',
-			monthly: '0%',
-			semester: '11%',
-			session: '33%',
-		},
-		{
-			key: '3',
-			row: 'Duration',
-			monthly: '1 Month',
-			semester: '6 Months',
-			session: '12 Months',
-		},
-	];
-	return (
-		<>
-			<section className={styles.sub_block}>
-				{/* <h3>Online Access</h3> */}
-				<Table
-					columns={columns}
-					dataSource={data}
-					bordered={true}
-					pagination={false}
-				/>
-				<Row gutter={8} className='mt-40'>
-					<Col className='gutter-row' xs={{ span: 24 }} lg={{ span: 12 }}>
-						<p className={styles.bold}>Benefits</p>
-						<ul>
-							<li>Free WEB and MOBILE APP</li>
-							<li>Weekly updates</li>
-							<li>Law Courses Research per School level</li>
-						</ul>
-					</Col>
-					<Col className='gutter-row' xs={{ span: 24 }} lg={{ span: 12 }}>
-						<p className={styles.bold}>Features</p>
-						<ul>
-							<li>Online Access to WRN Volumes and indexes</li>
-							<li>Supreme court Cases and Summaries</li>
-							<li>Court of Appeal Cases and Summaries</li>
-							<li>Laws of the Fed, Civil Procedure rules</li>
-							<li>Constitution</li>
-							<li> Law Dictionary</li>
-                            <li>Access to WRN Blog and resources.</li>
-						</ul>
-					</Col>
-				</Row>
-			</section>
+  const data = [
+    {
+      key: "1",
+      row: "Cost",
+      monthly: "NGN1,500",
+      semester: "NGN6,000",
+      session: "NGN10,000"
+    },
+    {
+      key: "2",
+      row: "Discount",
+      monthly: "0%",
+      semester: "11%",
+      session: "33%"
+    },
+    {
+      key: "3",
+      row: "Duration",
+      monthly: "1 Month",
+      semester: "6 Months",
+      session: "12 Months"
+    }
+  ];
+  return (
+    <>
+      <section className={styles.sub_block}>
+        {/* <h3>Online Access</h3> */}
+        <Table
+          columns={columns}
+          dataSource={data}
+          bordered={true}
+          pagination={false}
+          className={styles.table}
+        />
+        <Row gutter={8} className="mt-40">
+          <Col className="gutter-row" xs={{ span: 24 }} lg={{ span: 12 }}>
+            <p className={styles.bold}>Benefits</p>
+            <ul>
+              <li>Free WEB and MOBILE APP</li>
+              <li>Weekly updates</li>
+              <li>Law Courses Research per School level</li>
+            </ul>
+          </Col>
+          <Col className="gutter-row" xs={{ span: 24 }} lg={{ span: 12 }}>
+            <p className={styles.bold}>Features</p>
+            <ul>
+              <li>Online Access to WRN Volumes and indexes</li>
+              <li>Supreme court Cases and Summaries</li>
+              <li>Court of Appeal Cases and Summaries</li>
+              <li>Laws of the Fed, Civil Procedure rules</li>
+              <li>Constitution</li>
+              <li> Law Dictionary</li>
+              <li>Access to WRN Blog and resources.</li>
+            </ul>
+          </Col>
+        </Row>
+      </section>
 
-			{/* <section className={styles.sub_block}>
+      {/* <section className={styles.sub_block}>
 				<h3>Offline Access</h3>
 				<Table
 					columns={columns}
@@ -120,8 +121,8 @@ const Students = () => {
 					</Col>
 				</Row>
 			</section> */}
-		</>
-	);
+    </>
+  );
 };
 
 export default Students;
